fc6b508b0d8de2d40876c33e13af7a14b5ad207e
Abhay Sahani <abhay.cs.777@gmail.com>
2024-10-24 22:34:42 +0530

    vector emmebding using sentence transformer

---
+++ Generating Vector Embeddings/VectorEmbedding.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+df = pd.read_csv('output/analytics_vidhya_courses_with_ratings.csv')
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+course_descriptions = df['Description'].tolist()
+description_embeddings = model.encode(course_descriptions)
+
+# Store the embeddings into the DataFrame
+df['Embeddings'] = list(description_embeddings)
+
+# Save the DataFrame with embeddings for later use
+df.to_pickle('output/courses_with_embeddings.pkl')
